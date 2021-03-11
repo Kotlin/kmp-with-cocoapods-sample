02bd806758063ab691da680aabeadab34e687e32
Wojtek Zieba <wojtek.zieba@automattic.com>
2021-03-11 18:58:45 +0100

    Fix project not building
    - Include gradle-wrapper.jar file
    - Update Kotlin to latest stable version
    - Update Gradle Wrapper to latest stable version

--- kotlin-library/build.gradle.kts
+++ kotlin-library/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.4.255-SNAPSHOT"
-    kotlin("native.cocoapods") version "1.4.255-SNAPSHOT"
+    kotlin("multiplatform") version "1.4.31"
+    kotlin("native.cocoapods") version "1.4.31"
 }
 
 group = "org.jetbrains.kotlin.sample"
