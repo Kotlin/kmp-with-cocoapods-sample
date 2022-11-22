1f59e257aae3a47169c00d463630b3d998e8d72e
Artem Daugel-Dauge <artem.daugel-dauge@jetbrains.com>
2022-11-22 22:08:45 +0100

    Update versions, fix gradle wrapper

--- kotlin-library/src/iosX64Main/kotlin/A.kt
+++ kotlin-library/src/iosX64Main/kotlin/A.kt
@@ -2,14 +2,13 @@ import cocoapods.pod_dependency.*
 import cocoapods.SDWebImage.*
 import cocoapods.AFNetworking.*
 import cocoapods.subspec_dependency.*
-import cocoapods.cocoapodSourcesZip.cocoapodSourcesZip
 import cocoapods.example.foo as exampleFoo
 import cocoapods.CocoaLumberjack.*
 
 fun bar() {
     println(AFNetworkingReachabilityNotificationStatusItem)
-    println(cocoapodSourcesZip())
     println(foo())
+    println(CocoaLumberjackVersionNumber)
 }
 
 fun bazz() {
