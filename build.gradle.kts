39060518684848a7d4016e16dbf120120b5c0327
Andrey Yastrebov <andrey.yastrebov@jetbrains.com>
2025-05-05 10:56:28 +0200

    update sample

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
-    kotlin("multiplatform").version("2.0.0").apply(false)
-    kotlin("native.cocoapods").version("2.0.0").apply(false)
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinCocoapods).apply(false)
 }
--- kotlin-library/build.gradle.kts
+++ kotlin-library/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
-    kotlin("multiplatform")
-    kotlin("native.cocoapods")
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinCocoapods)
 }
 
 group = "org.jetbrains.kotlin.sample"
 version = "1.0-SNAPSHOT"
 
 kotlin {
-    iosX64()
     iosArm64()
     iosSimulatorArm64()
 
@@ -15,48 +14,62 @@ kotlin {
         summary = "Kotlin sample project with CocoaPods dependencies"
         homepage = "https://github.com/Kotlin/kotlin-with-cocoapods-sample"
 
-        ios.deploymentTarget = "13.5"
+        ios.deploymentTarget = "16.0"
 
-//      Example of usage remote Cocoapods dependency from Cocoapods repository
+        /**
+         * Example of usage remote Cocoapods dependency from Cocoapods repository
+         */
         pod("Base64") {
             version = "~> 1.1.2"
         }
 
-//      Example of usage remote Pod from Github repository by tag
+        /**
+         * Example of usage remote Pod from Github repository by tag
+         */
         pod("SDWebImage") {
             source = git("https://github.com/SDWebImage/SDWebImage.git") {
-                tag = "5.9.2"
+                tag = "5.21.0"
             }
         }
 
-//      Example of usage remote Pod from Github repository by branch
+        /**
+         * Example of usage remote Pod from Github repository by branch
+         */
         pod("JSONModel") {
             source = git("https://github.com/jsonmodel/jsonmodel.git") {
                 branch = "key-mapper-class"
             }
         }
 
-//      Example of usage remote Pod from Github repository by commit
-        pod("CocoaLumberjack") {
-            source = git("https://github.com/CocoaLumberjack/CocoaLumberjack.git") {
-                commit = "3e7f595e3a459c39b917aacf9856cd2a48c4dbf3"
+        /**
+         * Example of usage remote Pod from Github repository by commit
+         */
+        pod("AFNetworking") {
+            source = git("https://github.com/AFNetworking/AFNetworking.git") {
+                commit = "d9f589cc2c1fe9d55eb5eea00558010afea7a41e"
             }
         }
 
-//      Example of usage local Cocoapods dependency
+        /**
+         * Example of usage local Cocoapods dependency
+         */
         pod("pod_dependency") {
             version = "1.0"
             source = path(project.file("../pod_dependency"))
         }
 
-//      Example of usage local Pod declared as Subspec
+        /**
+         * Example of usage local Pod declared as Subspec
+         */
         pod("subspec_dependency/Core") {
             version = "1.0"
             source = path(project.file("../subspec_dependency"))
         }
 
-//      Example of usage Pod from custom spec repository
-//      Please, make sure that your existing Podfile contains `source 'path/to/spec-repo.git'` line
+        /**
+         * Example of usage Pod from custom spec repository
+         * Please, make sure that your existing Podfile contains `source 'path/to/spec-repo.git'` line
+         */
 //        specRepos {
 //            url("https://github.com/Kotlin/kotlin-cocoapods-spec.git")
 //        }
